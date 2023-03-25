d68e218a5217191922f7b8d88690fdbe09aa21f9
zaktwo <zaktwo@users.noreply.github.com>
2023-03-25 09:37:28 +0100

    update

---
+++ script.js
@@ -0,0 +1,13 @@
+const rating = document.querySelector(".radio-label");
+const submitBtn = document.querySelector(".submit");
+const ratingPoint = document.querySelector(".rating");
+const topCard = document.querySelector(".card");
+const thankYou = document.querySelector(".thankyou-card");
+
+let rate = null;
+
+rating.forEach((rating) => {
+    rating.addEventListener("click", (e) => {
+
+    });
+});
